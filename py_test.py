1ef91ee4648763003cd89628867e64bcf8a6c435
Rytual <rytuality@gmail.com>
2024-05-28 15:49:54 -0400

    Adding Sales Python Code for reference

--- py_test.py
+++ py_test.py
@@ -5,13 +5,13 @@
 #test
 print("rytest")
 
-
+data = [['tom', 10], ['nick', 15], ['juli', 14]]
 
 # Import pandas as pd
 import pandas as pd
 
 # Convert sales to a pandas DataFrame
-sales_df = pd.DataFrame(sales)
+sales_df = pd.DataFrame(data)
 
 # Preview the first five rows
 print(sales_df.head(5))

09d1f6abfd02240543c6489a38dc9d5dd16a0138
Rytual <rytuality@gmail.com>
2024-05-31 00:06:04 -0400

    Adding Sales Python Code for reference

---
+++ simple_math.py
@@ -0,0 +1,19 @@
+# Division
+print(100 / 2)
+
+print (3 / 9)
+
+# Addition
+print(50 + 10)
+
+print(88 + 12)
+
+# Multiplication
+print(10 * 60)
+
+print(14 * 74)
+
+# Subtraction
+print(89 - 73)
+
+print(636 - 353)
